e881d03b9c13f4f3c133c897ebe43614b40d3a5a
noirkotyara <53192867+noirkotyara@users.noreply.github.com>
2020-08-16 14:56:33 +0300

    JS navbar automatic collapse

---
+++ week_3/js/script.js
@@ -0,0 +1,39 @@
+$(function() { // Same as document.addEventListener("DOMContentLoaded"...
+
+    // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+    $('#navbarToggle').blur(function(event) {
+        if (window.innerWidth < 768) {
+            $('#collapsable-nav').collapse('hide');
+        }
+    });
+});
+
+(function(global) {
+    var dc = {};
+
+    var homeHtml = "snippets/home-snippet.html";
+
+    var insertHtml = function(selector, html) {
+        var targetElem = document.querySelector(selector);
+        targetElem.innerHTML = html;
+    };
+
+    var showLoading = function(selector) {
+        var html = "<div class='text-center'>";
+        html += "<img src='image/ajax-loader.gif'></div>";
+        insertHtml(selector, html);
+    };
+    document.addEventListener("DOMContentLoaded", function(event) {
+        showLoading("#main-content");
+        $ajaxUtils.sendGetRequest(
+            homeHtml,
+            function(responseText) {
+                document.querySelector("#main-content")
+                    .innerHTML = responseText;
+            },
+            false);
+    });
+
+
+    global.$dc = dc;
+})(window);
